74b3fd256a6b961fe1294a0a09065141397ab5fe
Sahil Patel <sahil1308patel@gmail.com>
2022-10-08 09:39:04 +0530

    Largest among 3

---
+++ Largest among 3.py
@@ -0,0 +1,16 @@
+
+a = int(input("Type a "))
+b = int(input("Type b "))
+c = int(input("Type c "))
+    
+    
+if (a > b):
+    if ( a > c):
+            print (a)
+    else:
+            print (c)
+            
+elif (c > b):
+        print (c)
+else:
+        print (b)
